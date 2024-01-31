eacfb9468e7d10af9bfa425dc77232211fc30d97
MichaelKrinsky <34221696+MichaelKrinsky@users.noreply.github.com>
2024-01-31 15:15:14 -0800

    Added routing stuffs

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import {createRoot} from "react-dom/client";
 import {Provider} from "react-redux";
 import {store} from "./redux/store";
-import App from "./App";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import "./index.css";
 import "./globalStyles.css";
 import {SolenoidPage} from "./solenoid_page/SolenoidPage";
@@ -29,8 +29,18 @@ setInterval(fetchAndDispatchRocketState, 1000);
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <SensorPage />
-            <App />
+            <Router>
+                <Routes>
+                    <Route
+                        path="/solenoids"
+                        element={<SolenoidPage />}
+                    />
+                    <Route
+                        path="/sensors"
+                        element={<SensorPage />}
+                    />
+                </Routes>
+            </Router>
         </Provider>
     </React.StrictMode>
 );
