9b2adc879f354dac2e0764f4bb68e2e622960f26
Kevin Wu <kevinwu098@gmail.com>
2024-10-17 09:53:59 -0700

    feat: actually update state

--- gui/src/useRocketTimestamps.jsx
+++ gui/src/useRocketTimestamps.jsx
@@ -1,23 +1,35 @@
 import {useState} from "react";
 
 export function useRocketTimestamps() {
-    // stores time_recv of last fresh packet
     const [lastGseTimeRecv, setLastGseTimeRecv] = useState(0);
     const [lastEcuTimeRecv, setLastEcuTimeRecv] = useState(0);
 
-    // stores Unix timestamp of last fresh packet
     const [lastGseTimestamp, setLastGseTimestamp] = useState(0);
     const [lastEcuTimestamp, setLastEcuTimestamp] = useState(0);
 
     const updateTimestamps = (timeRecv, systemName) => {
-        if (systemName === "gse" && timeRecv > lastGseTimeRecv) {
-            setLastGseTimeRecv(timeRecv);
-            setLastGseTimestamp(Date.now());
+        const now = Date.now();
+
+        if (systemName === "gse") {
+            setLastGseTimeRecv((prevLastGseTimeRecv) => {
+                if (timeRecv > prevLastGseTimeRecv) {
+                    setLastGseTimestamp(now);
+                    return timeRecv;
+                }
+
+                return prevLastGseTimeRecv;
+            });
         }
 
-        if (systemName === "ecu" && timeRecv > lastEcuTimeRecv) {
-            setLastEcuTimeRecv(timeRecv);
-            setLastEcuTimestamp(Date.now());
+        if (systemName === "ecu") {
+            setLastEcuTimeRecv((prevLastEcuTimeRecv) => {
+                if (timeRecv > prevLastEcuTimeRecv) {
+                    setLastEcuTimestamp(now);
+                    return timeRecv;
+                }
+
+                return prevLastEcuTimeRecv;
+            });
         }
     };
 
