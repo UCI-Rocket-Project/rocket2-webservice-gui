87534fd4d45179fde187f106d7ec9f13aedb231f
Melody Fang <mkfang@uci.edu>
2024-10-21 23:11:50 -0700

    changed layout of the page

--- gui/src/telemetry_page/TelemetryPage.js
+++ gui/src/telemetry_page/TelemetryPage.js
@@ -4,43 +4,62 @@ import RocketGauge from "../rocket_gauge/RocketGauge";
 import RocketSim from "../rocket_sim/RocketSim";
 export function TelemetryPage() {
     return (
-        <div className={styles.container}>
-            <RocketGauge
-                value={1000}
-                minValue={0}
-                maxValue={15000}
-                name={"Altitude"}
-                units={" ft"}
-            />
-            <RocketGauge
-                value={1000}
-                minValue={0}
-                maxValue={500}
-                name={"Speed"}
-                units={" mph"}
-                arc={{
-                    colorArray: ["#5BE12C", "#EA4228"],
-                    subArcs: [{limit: 100}, {limit: 300}, {}],
-                    padding: 0.02,
-                    width: 0.3
-                }}
-            />
-            <RocketGauge
-                value={0}
-                minValue={0}
-                maxValue={125}
-                name={"Y Acceleration"}
-                units={" ft/s²"}
-                arc={{
-                    colorArray: ["#5BE12C", "#EA4228"],
-                    subArcs: [{limit: 30}, {limit: 60}, {limit: 80}],
-                    padding: 0.02,
-                    width: 0.3
-                }}
-            />
-            <div>
-                <RocketSim color={"#ff0000"}></RocketSim>
+        <div className={styles.parent}>
+            <div className={styles.container}>
+                TELEMETRY PAGE
             </div>
+            <div className={styles.container}>
+                <div className={styles.GaugeCol}>
+                    <RocketGauge
+                        value={1000}
+                        minValue={0}
+                        maxValue={15000}
+                        name={"Altitude"}
+                        units={" ft"}
+                        arc={{
+                            colorArray: ["#5BE12C", "#EA4228"],
+                            subArcs: [{ limit: 100 }, { limit: 300 }, {}],
+                            padding: 0.02,
+                            width: 0.3
+                        }}
+                    />
+                    <RocketGauge
+                        value={1000}
+                        minValue={0}
+                        maxValue={500}
+                        name={"Speed"}
+                        units={" mph"}
+                        arc={{
+                            colorArray: ["#5BE12C", "#EA4228"],
+                            subArcs: [{ limit: 100 }, { limit: 300 }, {}],
+                            padding: 0.02,
+                            width: 0.3
+                        }}
+                    />
+                    <RocketGauge
+                        value={0}
+                        minValue={0}
+                        maxValue={125}
+                        name={"Y Acceleration"}
+                        units={" ft/s²"}
+                        arc={{
+                            colorArray: ["#5BE12C", "#EA4228"],
+                            subArcs: [{ limit: 30 }, { limit: 60 }, { limit: 80 }],
+                            padding: 0.02,
+                            width: 0.3
+                        }}
+                    />
+                </div>
+                <div className={styles.GaugeCol}>
+                    <div>
+                        <RocketSim color={"#ff0000"}></RocketSim>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 }
+
+
+
